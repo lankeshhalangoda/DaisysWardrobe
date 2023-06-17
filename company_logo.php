14f8e6411558f40979dc893a3c1394ccfd87094b
Lankesh Halangoda <lankeshhalangoda@gmail.com>
2023-06-18 02:19:35 +0530

    first commit

---
+++ company_logo.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+</head>
+<body>
+
+    <!-- Start Company Logo Section -->
+    <div style="margin-bottom: -100px; margin-top: 60px;" class="company-logo-section section-top-gap-100 section-fluid">
+        <div class="company-logo-wrapper" data-aos="fade-up" data-aos-delay="0">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="company-logo-slider default-slider-nav-arrow">
+                            <!-- Slider main container -->
+                            <div class="swiper-container company-logo-slider">
+                                
+                                <div class="swiper-wrapper">
+                                    <!-- Start Company Logo Single Item -->
+                                    <div class="company-logo-single-item swiper-slide">
+                                        <div style="width: 100%; object-fit: cover;" class="image"><img class="img-fluid" src="assets/images/company-logo/lacoste.jpg" alt="lacoste"></div>
+                                    </div>
+                                    <!-- End Company Logo Single Item -->
+                                    <!-- Start Company Logo Single Item -->
+                                    <div class="company-logo-single-item swiper-slide">
+                                        <div style="width: 100%; object-fit: cover;" class="image"><img class="img-fluid" src="assets/images/company-logo/moose.jpg" alt="moose"></div>
+                                    </div>
+                                    <!-- End Company Logo Single Item -->
+                                    <!-- Start Company Logo Single Item -->
+                                    <div class="company-logo-single-item swiper-slide">
+                                        <div style="width: 100%; object-fit: cover;" class="image"><img class="img-fluid" src="assets/images/company-logo/licc.jpg" alt="licc"></div>
+                                    </div>
+                                    <!-- End Company Logo Single Item -->
+                                    <!-- Start Company Logo Single Item -->
+                                    <div class="company-logo-single-item swiper-slide">
+                                        <div style="width: 100%; object-fit: cover;" class="image"><img class="img-fluid" src="assets/images/company-logo/emerald.png" alt="emerald"></div>
+                                    </div>
+                                    <!-- End Company Logo Single Item -->
+                                    <!-- Start Company Logo Single Item -->
+                                    <div class="company-logo-single-item swiper-slide">
+                                        <div style="width: 100%; object-fit: cover;" class="image"><img class="img-fluid" src="assets/images/company-logo/levis.png" alt="levis"></div>
+                                    </div>
+                                    <!-- End Company Logo Single Item -->
+                                    <!-- Start Company Logo Single Item -->
+                                    <div class="company-logo-single-item swiper-slide">
+                                        <div style="width: 100%; object-fit: cover;" class="image"><img class="img-fluid" src="assets/images/company-logo/holister.png" alt="holister"></div>
+                                    </div>
+                                    <!-- End Company Logo Single Item -->
+                                    <!-- Start Company Logo Single Item -->
+                                    <div class="company-logo-single-item swiper-slide">
+                                        <div style="width: 100%; object-fit: cover;" class="image"><img class="img-fluid" src="assets/images/company-logo/Adidas-Logo.wine (1).svg" alt="adidas"></div>
+                                    </div>
+                                    <!-- End Company Logo Single Item -->
+                                    <!-- Start Company Logo Single Item -->
+                                    <div class="company-logo-single-item swiper-slide">
+                                        <div style="width: 100%; object-fit: cover;" class="image"><img class="img-fluid" src="assets/images/company-logo/bench.svg" alt="bench"></div>
+                                    </div>
+                                    <!-- End Company Logo Single Item -->
+                                    <!-- Start Company Logo Single Item -->
+                                    <div class="company-logo-single-item swiper-slide">
+                                        <div style="width: 100%; object-fit: cover;" class="image"><img class="img-fluid" src="assets/images/company-logo/nike.png" alt="nike"></div>
+                                    </div>
+                                    <!-- End Company Logo Single Item -->
+                                    <!-- Start Company Logo Single Item -->
+                                    <div class="company-logo-single-item swiper-slide">
+                                        <div style="width: 100%; object-fit: cover;" class="image"><img class="img-fluid" src="assets/images/company-logo/puma.png" alt="puma"></div>
+                                    </div>
+                                    <!-- End Company Logo Single Item -->
+                                    <!-- Start Company Logo Single Item -->
+                                    <div class="company-logo-single-item swiper-slide">
+                                        <div style="width: 100%; object-fit: cover;" class="image"><img class="img-fluid" src="assets/images/company-logo/heybo.jpg" alt="heybo"></div>
+                                    </div>
+                                    <!-- End Company Logo Single Item -->
+                                    <!-- Start Company Logo Single Item -->
+                                    <div class="company-logo-single-item swiper-slide">
+                                        <div style="width: 100%; object-fit: cover;" class="image"><img class="img-fluid" src="assets/images/company-logo/mas.webp" alt="mas"></div>
+                                    </div>
+                                    <!-- End Company Logo Single Item -->
+                                </div>
+                            </div>
+                            
+                            <div class="swiper-button-prev d-none d-md-block"></div>
+                            <div class="swiper-button-next d-none d-md-block"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End Company Logo Section -->
+    
+</body>
+</html>
